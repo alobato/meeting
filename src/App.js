72dba0841bfbf24f4b90eef05fc8e52a257cf912
Arley Lobato <arleylobato@gmail.com>
2020-06-15 09:06:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import useSWR from 'swr'
 
 function App() {
+  const fetcher = url => fetch(url).then(r => r.json())
+  const { data, error } = useSWR('https://covid19-brazil-api.now.sh/api/report/v1', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.data.map(item => (
+        <div style={{marginBottom: 32}}>
+          <div>UF: {item.uf}</div>
+          <div>Estado: {item.state}</div>
+          <div>Casos: {item.cases}</div>
+          <div>Suspeitos: {item.suspects}</div>
+          <div>Mortes: {item.deaths}</div>
+        </div>
+      ))}
     </div>
-  );
+  )
+
+
 }
 
-export default App;
+export default App
